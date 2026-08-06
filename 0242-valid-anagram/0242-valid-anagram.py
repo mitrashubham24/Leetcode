6ffff8895eb3d5cd8f977916b4dd515e785fd594
mitrashubham24 <mitrashubham24@gmail.com>
2026-08-07 01:22:29 +0530

    Time: 20 ms (56.43%), Space: 13.3 MB (40.59%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+
+        if len(s) != len(t):
+            return False
+
+        if sorted(s) == sorted(t):
+            return True
+
+        return False
