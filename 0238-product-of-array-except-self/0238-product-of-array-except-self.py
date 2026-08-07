b87382588e906e188c25386c13d5a6f851e117f1
mitrashubham24 <mitrashubham24@gmail.com>
2026-08-08 02:08:05 +0530

    Time: 29 ms (72.39%), Space: 20.1 MB (72.62%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+
+        n = len(nums)
+
+        answer = [1] * n
+
+        left = 1
+
+        for i in range(n):
+            answer[i] = left
+            left = left * nums[i]
+
+        right = 1
+
+        for i in range(n - 1, -1, -1):
+            answer[i] = answer[i] * right
+            right = right * nums[i]
+
+        return answer
